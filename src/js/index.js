74d1682bb489ec949af645f1cae124cbd433e0ea
Eremin Maxim <enflaer@gmail.com>
2019-08-14 12:17:40 +0500

    updated babel, webpack, dependencies, added new scss templates, js new scripts, added new pug components

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,134 @@
-import jQuery from 'jquery';
-import popper from 'popper.js';
-import bootstrap from 'bootstrap';
+import $ from 'jquery';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+  // smooth anchors
+  
+  let linkNav = document.querySelectorAll('[href^="#"]'),
+    V = 0.25;
+  for (let i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      let w = window.pageYOffset,
+        hash = this.href.replace(/[^#]*(.*)/, '$1'),
+        t = document.querySelector(hash).getBoundingClientRect().top,
+        start = null;
+      requestAnimationFrame(step);
+      
+      function step(time) {
+        if (start === null) start = time;
+        let progress = time - start,
+          r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+        window.scrollTo(0, r);
+        if (r !== w + t) {
+          requestAnimationFrame(step)
+        } else {
+          location.hash = hash;
+        }
+      }
+    }, false);
+  }
+  
+  // form submit
+  
+  let formSubmit = $("#js-submit-mailto"),
+    form = $('.modal');
+  
+  $(document).ready(function () {
+    formSubmit.click(
+      function () {
+        sendAjaxForm('result-form', 'ajax-form', 'send.php');
+        return false;
+      }
+    );
+  });
+  
+  function sendAjaxForm(result_form, ajax_form, url) {
+    $.ajax({
+      url: url,
+      type: "POST",
+      dataType: "html",
+      data: $("#" + ajax_form).serialize(),
+      success: function (response) {
+        let result = $.parseJSON(response);
+        form.addClass('success');
+        setTimeout(function () {
+          form.removeClass('success');
+        }, 3000);
+      },
+      error: function (response) {
+        document.getElementById(result_form).innerHTML = "Ошибка. Данные не отправленны.";
+        form.addClass('error');
+        setTimeout(function () {
+          form.removeClass('error');
+        }, 3000);
+      }
+    });
+  }
+  
+  // modal
+  
+  let btnJs = document.querySelector('.btn-js');
+  
+  class Modal {
+    constructor(dataId) {
+      this.modal = document.querySelector('.modal[data-modal="' + dataId + '"]');
+      
+      this.modalBg = document.querySelector('.modal-bg');
+      
+      this.modalClose = document.querySelector('.modal-close');
+      
+      this.modalBg.addEventListener('click', () => {
+        this.modalToggle();
+      });
+      
+      this.modalClose.addEventListener('click', () => {
+        this.modalToggle();
+      });
+      
+    }
+    
+    modalToggle() {
+      this.modal.classList.toggle('active');
+      this.modalBg.classList.toggle('active');
+    };
+  }
+  
+  let modalMail = new Modal('mailto');
+  
+  btnJs.addEventListener('click', function () {
+    modalMail.modalToggle();
+  });
+  
+  // youtube lazy loading
+  
+  let youtube = [...document.querySelectorAll( ".youtube" )];
+  
+  if (youtube.length > 0) {
+    youtube.forEach( (el, index) => {
+      
+      let source = "https://img.youtube.com/vi/"+ youtube[index].dataset.embed +"/sddefault.jpg";
+      
+      let image = new Image();
+      image.src = source;
+      image.addEventListener( "load", function() {
+        youtube[index].appendChild( image );
+      }(index) );
+      
+      youtube[index].addEventListener( "click", function() {
+        
+        let iframe = document.createElement( "iframe" );
+        
+        iframe.setAttribute( "frameborder", "0" );
+        iframe.setAttribute( "allowfullscreen", "" );
+        iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+        
+        this.innerHTML = "";
+        this.appendChild( iframe );
+      } );
+    })
+  }
+  
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -6,127 +6,153 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-
-const devMode = process.env.NODE_ENV !== 'production';
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 let pluginsOptions = [
-    new CleanWebpackPlugin(),
-    new WebpackMd5Hash(),
-    new MiniCssExtractPlugin({
-        filename: './css/[name].css',
-        chunkFilename: './css/[id].css',
-    }),
-    new CopyWebpackPlugin([{
-        from: './src/fonts',
-        to: './fonts'
+  new CleanWebpackPlugin(),
+  new WebpackMd5Hash(),
+  new MiniCssExtractPlugin({
+    filename: './css/[name].css',
+    chunkFilename: './css/[id].css',
+  }),
+
+  new CopyWebpackPlugin([{
+    from: './src/fonts',
+    to: './fonts'
+  },
+    {
+      from: './src/favicon',
+      to: './favicon'
     },
-        {
-            from: './src/favicon',
-            to: './favicon'
-        },
-        {
-            from: './src/img',
-            to: './img'
-        },
-    ]),
+    {
+      from: './src/img',
+      to: './img'
+    },
+    {
+      from: './src/send.php',
+      to: './'
+    },
+  ]),
+  new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
 ];
 
-let pages = glob.sync(__dirname + '/src/pug/*.pug');
+let pages = glob.sync(__dirname + '/src/*.pug');
 pages.forEach(function (file) {
-    let base = path.basename(file, '.pug');
-    pluginsOptions.push(
-        new HtmlWebpackPlugin({
-            filename: './' + base + '.html',
-            template: './src/pug/' + base + '.pug',
-            inject: true,
-        })
-    )
+  let base = path.basename(file, '.pug');
+  pluginsOptions.push(
+    new HtmlWebpackPlugin({
+      filename: './' + base + '.html',
+      template: './src/' + base + '.pug',
+      inject: true,
+    })
+  )
 });
 
-module.exports = {
-    entry: ['./src/js/index.js', './src/scss/main.scss'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: devMode ? '[name].js' : '[name].[hash].js',
-        publicPath: "/"
-    },
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.pug$/,
-                exclude: ['/node_modules/', '/src/pug/partials'],
-                loader: 'pug-loader',
-                query: {
-                    pretty: true
-                }
+module.exports = (env, argv) => ({
+  entry: ['./src/js/index.js', './src/scss/main.scss'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: argv.mode === 'development' ? '[name].js' : '[name].js',
+    publicPath: "/"
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        exclude: ['/node_modules/', '/src/pug/partials'],
+        loader: 'pug-loader',
+        query: {
+          pretty: true
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /node_modules\/(?!(dom7|swiper)\/).*/] ,
+        include: path.resolve(__dirname, 'src/js'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [["minify", {
+              "builtIns": false
+            }]],
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: argv.mode === 'development',
             },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                include: path.resolve(__dirname, 'src/js'),
-                use: {
-                    loader: 'babel-loader',
-                }
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: process.env.NODE_ENV === 'development',
-                        },
-                    },
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-            {
-                test: /\.(woff|woff2)$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 4096,
-                    name: './fonts/[name].[ext]?[hash]', // was '/fonts/[name].[ext]?[hash]',
-                },
-            },
-        ]
-    },
-    plugins: pluginsOptions,
-    // ,optimization: {
-    //     minimizer: [new TerserPlugin({
-    //         test: /\.(js)(\?.*)?$/i,
-    //     })],
-    // }
-    stats: {
-        colors: true,
-        hash: true,
-        version: true,
-        timings: true,
-        assets: false,
-        chunks: false,
-        modules: false,
-        reasons: false,
-        children: false,
-        source: false,
-        errors: true,
-        errorDetails: true,
-        warnings: true,
-        publicPath: false
-    },
-    devServer: {
-        clientLogLevel: 'warning',
-        historyApiFallback: true,
-        hot: true,
-        publicPath: "/",
-        inline: true,
-        overlay: true,
-        contentBase: 'dist',
-        host: 'localhost',
-        port: 9005
-    },
-};
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 4096,
+          name: './fonts/[name].[ext]?[hash]', // was '/fonts/[name].[ext]?[hash]',
+        },
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        // Specify enforce: 'pre' to apply the loader
+        // before url-loader/svg-url-loader
+        // and not duplicate it in rules with them
+        enforce: 'pre'
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10 * 1024
+        }
+      }
+    ]
+  },
+  plugins: pluginsOptions,
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  stats: {
+    colors: true,
+    hash: true,
+    version: true,
+    timings: true,
+    assets: false,
+    chunks: false,
+    modules: false,
+    reasons: false,
+    children: false,
+    source: false,
+    errors: true,
+    errorDetails: true,
+    warnings: true,
+    publicPath: false
+  },
+  devServer: {
+    clientLogLevel: 'warning',
+    historyApiFallback: true,
+    hot: true,
+    publicPath: "/",
+    inline: true,
+    overlay: true,
+    contentBase: 'dist',
+    host: 'localhost',
+    port: 9006
+  },
+});
